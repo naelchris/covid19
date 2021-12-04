ab4e2bb263cbc348766c69734eb89e49a7c25c22
koema8 <delvin.chianardi@tokopedia.com>
2021-12-04 23:33:27 +0700

    [Register] Register Usecase and Middleware
    - Register usecase
    - Login and Register Middleware
    - fix somebug and testing the login also another thing that needed to check in auth

--- server/http/auth/type.go
+++ server/http/auth/type.go
@@ -10,13 +10,12 @@ const LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
 var JWT_SIGNATURE_KEY = []byte("the secret of kalimdor")
 
 type loginRequest struct {
-	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type UserClaims struct {
 	jwt.StandardClaims
-	UserID       int64     `json:"user_id"`
 	Name         string    `json:"name"`
 	Email        string    `json:"email"`
 	DateOfBirth  time.Time `json:"date_of_birth"`
